f9bd18429b65cdd55c651ed9156c63240e809636
Arni Sturluson <sturlusa@oregonstate.edu>
2017-11-14 15:37:39 -0800

    Fix conversion matrices. Add exploreframe & rotate

--- Crystal.jl
+++ Crystal.jl
@@ -47,71 +47,171 @@ end # Framework end
 
 Read a .cssr file and construct a Framework object
 """
-function constructframework(cssrfilename::String)
-    f = open(cssrfilename, "r")
+function constructframework(filename::String)
+    extension = split(filename,".")[end]
+    f = open(filename, "r")
     lines = readlines(f)
+    # This reader is a mishmash of a lot of things. This needs to be cleaned up!
 
-    # Initialize variables
-    n_atoms = length(lines) - 5
-    a, b, c, α, β, γ = Array{Float64}(6)
-    x = Array{Float64}(n_atoms) # fractional
-    y = similar(x)
-    z = similar(x)
-    atoms = Array{String}(n_atoms)
 
-    # Make a boolean variable to fix discrepency with fractional and cartesian coordinates in cssr files
-    corr = false
-    for line in lines[6:6+16]
-        str = split(line)
-        for val in str[3:5]
-            # Check if values are fractional or not. I only check 10 lines, which is probably enough.
-            if parse(Float64,val)>1
-                corr = true
-                break
+    if extension == "cssr"
+        # Initialize variables
+        charges = Float64[]
+        n_atoms = length(lines) - 5
+        a, b, c, α, β, γ = Array{Float64}(6)
+        x = Array{Float64}(n_atoms) # fractional
+        y = similar(x)
+        z = similar(x)
+        atoms = Array{String}(n_atoms)
+
+        # Make a boolean variable to fix discrepency with fractional and cartesian coordinates in cssr files
+        corr = false
+        for line in lines[6:6+16]
+            str = split(line)
+            for val in str[3:5]
+                # Check if values are fractional or not. I only check 10 lines, which is probably enough.
+                if parse(Float64,val)>1
+                    corr = true
+                    break
+                end
+            end
+        end
+
+        # Iterate through the lines of the cssr file.
+        for (i,line) in enumerate(lines)
+            str = split(line)
+            # Unit cell dimension line
+            if i == 1
+                a, b, c = map(x->parse(Float64, x), str[end-2:end])
+            # Unit cell angle line
+            elseif i == 2
+                temp = zeros(3)
+                cnt = 1
+                for val in str
+                    try
+                        temp[cnt] = parse(Float64,val)*π/180
+                        cnt += 1
+                    end
+                end
+                α, β, γ = temp[1:3]
+            # Atom lines
+            elseif i > 5
+                try # Fix faulty cssr files where columns one and two merge for some reason
+                    parse(Float64,str[1])
+                catch
+                    str = correct_cssr_line(str)
+                end
+
+                # Fix element column where some cssr files have a number concated to the end of the element tag (e.g. H3)
+                tempch = ""
+                for ch in str[2]
+                    if !isdigit(ch)
+                        tempch = string(tempch,ch)
+                    end
+                end
+                atoms[i - 5] = tempch
+                # Use the correction boolean to fix fractional/cartesian discrepency
+                if corr
+                    x[i - 5], y[i - 5], z[i - 5] = map(x->parse(Float64, x), str[3:5])./[a, b, c]
+    				if i == 6
+    					@printf("x = %f, y = %f, z = %f\n",x[i-5],y[i-5],z[i-5])
+    				end
+                else
+                    x[i - 5], y[i - 5], z[i - 5] = map(x->parse(Float64, x), str[3:5])
+                end
             end
         end
     end
 
-    # Iterate through the lines of the cssr file.
-    for (i,line) in enumerate(lines)
-        str = split(line)
-        # Unit cell dimension line
-        if i == 1
-            a, b, c = map(x->parse(Float64, x), str[end-2:end])
-        # Unit cell angle line
-        elseif i == 2
-            temp = zeros(3)
-            cnt = 1
-            for val in str
-                try
-                    temp[cnt] = parse(Float64,val)*π/180
-                    cnt += 1
-                end
+    # Cif reader from Cory Simon
+    if extension == "cif"
+        data = Dict()
+        charges = Float64[]
+        a, b, c, α, β, γ = Array{Float64}(6)
+        x = Float64[]
+        y = Float64[]
+        z = Float64[]
+        atoms = String[]
+
+        loop_starts = -1
+        for i = 1:length(lines)
+            line = split(lines[i])
+            if length(line) == 0
+                continue
             end
-            α, β, γ = temp[1:3]
-        # Atom lines
-        elseif i > 5
-            try # Fix faulty cssr files where columns one and two merge for some reason
-                parse(Float64,str[1])
-            catch
-                str = correct_cssr_line(str)
+            if line[1] == "_symmetry_space_group_name_H-M"
+                if length(line) == 3
+                    @assert(contains(line[2] * line[3], "P1") || contains(line[2] * line[3], "P 1"), ".cif must have P1 symmetry.\n")
+                elseif length(line) == 2
+                    @assert(contains(line[2], "P1") || contains(line[2], "P 1"), ".cif must have P1 symmetry\n")
+                else
+                    println(line)
+                    error("Does this .cif have P1 symmetry?")
+                end
             end
 
-            # Fix element column where some cssr files have a number concated to the end of the element tag (e.g. H3)
-            tempch = ""
-            for ch in str[2]
-                if !isdigit(ch)
-                    tempch = string(tempch,ch)
+            for axis in ["a", "b", "c"]
+                if line[1] == @sprintf("_cell_length_%s", axis)
+                    data[axis] = parse(Float64, line[2])
                 end
             end
-            atoms[i - 5] = tempch
-            # Use the correction boolean to fix fractional/cartesian discrepency
-            if corr
-                x[i - 5], y[i - 5], z[i - 5] = map(x->parse(Float64, x), str[3:5])./[a, b, c]
+            for angle in ["alpha", "beta", "gamma"]
+                if line[1] == @sprintf("_cell_angle_%s", angle)
+                    data[angle] = parse(Float64, line[2]) * pi / 180.0
+                end
+            end
+
+            if (line[1] == "loop_")
+                next_line = split(lines[i+1])
+                if contains(next_line[1], "_atom_site")
+                    loop_starts = i + 1
+                    break
+                end
+            end
+        end  # end loop over lines
+
+        if loop_starts == -1
+            error("Could not find _atom_site* after loop_ if .cif file\n")
+        end
+
+        # broke the loop. so loop_starts is line where "_loop" first starts
+        # name_to_column is a dictionary that e.g. returns which column contains x fractional coord
+        #   use example: name_to_column["_atom_site_fract_x"] gives 3
+        atom_column_name = "_atom_site_type_symbol"  # this can be different for different .cifs...
+        name_to_column = Dict{AbstractString, Int}()
+        i = loop_starts
+        while length(split(lines[i])) == 1
+            if i == loop_starts
+                atom_column_name = split(lines[i])[1]
+            end
+            name_to_column[split(lines[i])[1]] = i + 1 - loop_starts
+            i += 1
+        end
+
+        # now extract fractional coords of atoms and their charges
+        for i = loop_starts+length(name_to_column):length(lines)
+            line = split(lines[i])
+            if length(line) != length(name_to_column)
+                break
+            end
+            push!(atoms, line[name_to_column[atom_column_name]])
+            push!(x, mod(parse(Float64, line[name_to_column["_atom_site_fract_x"]]), 1.0))
+            push!(y, mod(parse(Float64, line[name_to_column["_atom_site_fract_y"]]), 1.0))
+            push!(z, mod(parse(Float64, line[name_to_column["_atom_site_fract_z"]]), 1.0))
+            # if charges present, import them
+            if haskey(name_to_column, "_atom_site_charge")
+                push!(charges, parse(Float64, line[name_to_column["_atom_site_charge"]]))
             else
-                x[i - 5], y[i - 5], z[i - 5] = map(x->parse(Float64, x), str[3:5])
+                push!(charges, 0.0)
             end
         end
+        n_atoms = length(x)
+        a = data["a"]
+        b = data["b"]
+        c = data["c"]
+        α = data["alpha"]
+        β = data["beta"]
+        γ = data["gamma"]
     end
     close(f)
 
--- Forcefield.jl
+++ Forcefield.jl
@@ -4,7 +4,7 @@ module Forcefield
 using Crystal
 using Mols
 
-export LennardJonesForceField, constructforcefield, lennard_jones, readproperties, rep_factors, centerofmass, vdw_energy
+export LennardJonesForceField, constructforcefield, lennard_jones, readproperties, rep_factors, centerofmass, vdw_energy, rotate, exploreframe
 
 const NA = 6.022e23 # 1/mol
 const R = 1.9872036e-3 # kcal/(mol K)
@@ -16,17 +16,17 @@ Data structure for a Lennard Jones forcefield, read from a file containing UFF p
 
 # Arguments
 - `cutoffradius::Float64`: cut-off radius to define where the potential energy goes to zero, rather than taking the limit as r → ∞ (units: Angstrom)
-- `epsilon_dict::Dict{String, Float64}`: Dictionary that connects element acronyms to an ϵ, which is the depth of a Lennard Jones potential well
-- `sigma_dict::Dict{String, Float64}`: Dictionary that connects element acronyms to a σ, which is the finite distance where the potential between atoms goes to zero
-- `atom_to_id::Dict{String, Int64}`: Dictionary that connects element acronyms to a unique integer value.
+- `epsilon_dict::Dict{AbstractString, Float64}`: Dictionary that connects element acronyms to an ϵ, which is the depth of a Lennard Jones potential well
+- `sigma_dict::Dict{AbstractString, Float64}`: Dictionary that connects element acronyms to a σ, which is the finite distance where the potential between atoms goes to zero
+- `atom_to_id::Dict{AbstractString, Int64}`: Dictionary that connects element acronyms to a unique integer value.
 - `epsilons::Array{Float64,2}`: Two dimensional matrix that contains the ϵ interacting values between two elements. Row/Column number correspond to id's from atom_to_id (units: kcal/mol)
 - `sigmas::Array{Float64,2}`: Two dimensional matrix that contains the σ interacting values between two elements. Row/Column number correspond to id's from atom_to_id (units: Angstrom)
 """
 struct LennardJonesForceField
 	cutoffradius::Float64
-	epsilon_dict::Dict{String, Float64}
-	sigma_dict::Dict{String, Float64}
-	atom_to_id::Dict{String, Int64}
+	epsilon_dict::Dict{AbstractString, Float64}
+	sigma_dict::Dict{AbstractString, Float64}
+	atom_to_id::Dict{AbstractString, Int64}
 	epsilons::Array{Float64, 2}
 	sigmas::Array{Float64,2}
 end
@@ -36,16 +36,16 @@ end
 
 Read a .csv file containing UFF parameters (with the following column order: [Element, σ, ϵ]) and constructs a LennardJonesForceField object
 """
-function constructforcefield(filename::String; cutoffradius::Float64=14.0)
+function constructforcefield(filename::AbstractString; cutoffradius::Float64=14.0)
 	f = open(filename,"r")
 	lines = readlines(f)
 
 	n_ele = length(lines)-1
 
-	epsilon_dict = Dict{String, Float64}()
-	sigma_dict = Dict{String, Float64}()
-	atom_to_id = Dict{String, Int64}()
-	elements = Array{String}(n_ele)
+	epsilon_dict = Dict{AbstractString, Float64}()
+	sigma_dict = Dict{AbstractString, Float64}()
+	atom_to_id = Dict{AbstractString, Int64}()
+	elements = Array{AbstractString}(n_ele)
 	epsilons = Array{Float64,2}(n_ele,n_ele)
 	sigmas = similar(epsilons)
 
@@ -80,6 +80,9 @@ Calculate the lennard jones potential energy given a radius r between two molecu
 """
 function lennard_jones(r::Float64, σ::Float64, ϵ::Float64 )
 	ratio = (σ/r)^6
+#	if σ > r
+#		@printf("σ = %f, r = %f\n",σ,r)
+#	end
 	return 4*ϵ*(ratio^2 - ratio)
 end # lennard_jones end
 
@@ -135,8 +138,8 @@ end # end rep_factors
 Reads a .csv file with the following header: Element name, Molecular mass, Atomic radius, Ionic radius and returns a dictionary with an element name pointing at an array of values,
 EleProps[Element] => [Molecular mass(amu), Atomic radius(Angstrom), Ionic radius(Angstrom)]
 """
-function readproperties(filename::String)
-	EleProps = Dict{String,Array{Float64}}()
+function readproperties(filename::AbstractString)
+	EleProps = Dict{AbstractString,Array{Float64}}()
 	f = open(filename,"r")
 	lines = readlines(f)
 
@@ -165,7 +168,7 @@ end # end readproperties
 Uses `readproperties` to get a dictionary of element properties and uses that to calculate the center of mass of a supercell made from Framework and replication factors. (See readElementProps for more info on that function)
 Calculates the center of mass according to r_cm = ∑r_i*m_i/m_tot in fractional coordinates but returns in cartesian coordinates
 """
-function centerofmass(frame::Framework, filename::String, rep_factors::Array{Int64})
+function centerofmass(frame::Framework, filename::AbstractString, rep_factors::Array{Int64})
 	repA = rep_factors[1]
 	repB = rep_factors[2]
 	repC = rep_factors[3]
@@ -195,28 +198,35 @@ function vdw_energy(frame::Framework, molecule::Molecule, ljforcefield::LennardJ
 	σ = 0.0
 	r = 0.0
 	ϵ = 0.0
+	fpos = frame.C_to_f*pos
 	potsum = 0
 	for nA=0:repA-1, nB=0:repB-1, nC=0:repC-1
 		for i=1:molecule.n_atoms
 			for k=1:frame.n_atoms
 				# Nearest image convention. If the interaction between the probe molecule and atom k is being looked at, we'll only look at the interaction between the probe molecule and the closest replication of atom k. This is done with fractional coordinates for simplication and transformation to cartesian is done later.
 				repvec = [nA, nB, nC]
-				if abs(pos[1]+molecule.x[i,1]-(frame.f_coords[k,1]+nA)) > repA/2
+				if abs(fpos[1]+molecule.x[i,1]-(frame.f_coords[k,1]+nA)) > repA/2
 					repvec -= [repA,0,0]
 				end
-				if abs(pos[2]+molecule.x[i,2]-(frame.f_coords[k,2]+nB)) > repB/2
+				if abs(fpos[2]+molecule.x[i,2]-(frame.f_coords[k,2]+nB)) > repB/2
 					repvec -= [0,repB,0]
 				end
-				if abs(pos[3]+molecule.x[i,3]-(frame.f_coords[k,3]+nC)) > repC/2
+				if abs(fpos[3]+molecule.x[i,3]-(frame.f_coords[k,3]+nC)) > repC/2
 					repvec -= [0,0,repC]
 				end
 #				println(repvec)
 #				println("==========================\n")
 
+				temp = frame.f_to_C*(frame.f_coords[k,:]+repvec)
 				r = vecnorm( (pos+molecule.x[i,:]) - frame.f_to_C*(frame.f_coords[k,:]+repvec) )
 				σ = ljforcefield.sigmas[ ljforcefield.atom_to_id[ frame.atoms[k] ] , ljforcefield.atom_to_id[ molecule.atoms[i] ] ]
 				ϵ = ljforcefield.epsilons[ ljforcefield.atom_to_id[ frame.atoms[k] ] , ljforcefield.atom_to_id[ molecule.atoms[i] ] ]
 				if (r < ljforcefield.cutoffradius)
+#					@printf("Calling lennard_jones(%f,%f,%f)\n",r,σ,ϵ)
+					if lennard_jones(r,σ,ϵ) > 0
+#						@printf("%s-%d (nA = %d, nB = %d, nC = %d) and %s-%d -> r = %f | pos = [%f,%f,%f]\n",frame.atoms[k],k,nA,nB,nC,molecule.atoms[i],i,r,pos[1],pos[2],pos[3])
+					@printf("pos = [%f,%f,%f], frame.f_to_C*(frame.f_coords[k,:]+repvec) = [%f,%f,%f]\n",pos[1],pos[2],pos[3],temp[1],temp[2],temp[3])
+					end
 					potsum += lennard_jones(r,σ,ϵ)
 				end
 			end
@@ -225,4 +235,42 @@ function vdw_energy(frame::Framework, molecule::Molecule, ljforcefield::LennardJ
 	return potsum
 end # vdw_energy end
 
+"""
+	rotate!(rotMatrix::Array{Float64,2},θ::Float64,ϕ::Float64,ψ::Float64)
+
+Changes the rotation matrix `rotMatrix` to a rotation defined by the angles θ,ϕ,ψ (see http://mathworld.wolfram.com/EulerAngles.html)
+"""
+function rotate(rotMatrix::Array{Float64,2},θ::Float64,ϕ::Float64,ψ::Float64)
+	B = [cos(ψ) sin(ψ) 0; -sin(ψ) cos(ψ) 0; 0 0 1]
+	C = [1 0 0; 0 cos(θ) sin(θ); 0 -sin(θ) cos(θ)]
+	D = [cos(ϕ) sin(ϕ) 0; -sin(ϕ) cos(ϕ) 0; 0 0 1]
+	rotMatrix = B*C*D
+end
+
+function exploreframe(frame::Framework, molecule::Molecule, ljforcefield::LennardJonesForceField, repfactors::Array{Int64}; rotation=false, mesh=101)
+	if rotation
+		rotMatrix = zeros(3,3)
+		EnergyMatrix = zeros(mesh,mesh,mesh,mesh)
+		# How should I determine what rotations to use?
+		rotcnt = 10
+	else
+		EnergyMatrix = zeros(mesh,mesh,mesh)
+		rotcnt = 1
+	end
+	coordmatrix = Dict{AbstractString,Array{Float64,1}}()
+	frac_range_x = linspace(0,repfactors[1],mesh)	
+	frac_range_y = linspace(0,repfactors[2],mesh)	
+	frac_range_z = linspace(0,repfactors[3],mesh)	
+
+	for (i,xf) in enumerate(frac_range_x), (j,yf) in enumerate(frac_range_y), (k,zf) in enumerate(frac_range_z)
+		pos = frame.f_to_C*[xf, yf, zf]
+#		@printf("pos = [%f, %f, %f]\n",pos[1],pos[2],pos[3])
+#		@printf("i = %d, j = %d, k = %d\n",i,j,k)
+		EnergyMatrix[i,j,k] = vdw_energy(frame, molecule, ljforcefield, pos, repfactors)	
+		tempstr = @sprintf("%d-%d-%d",i,j,k)
+		coordmatrix[tempstr]=pos
+	end
+	println(typeof(coordmatrix))
+	return (EnergyMatrix,coordmatrix)
+end
 end # end module
