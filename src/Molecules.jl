b46b792c51075ba763d24194538a878cdec11732
Arni Sturluson <sturlusa@oregonstate.edu>
2017-11-21 15:06:40 -0800

    String -> AbstractString, and minor fixes

--- src/Molecules.jl
+++ src/Molecules.jl
@@ -5,13 +5,13 @@ Data structure for a molecule/adsorbate.
 
 # Arguments
 - `n_atoms::Int64`: Number of atoms in the molecule
-- `atoms::Array{String}`: List of (pseudo)atoms
+- `atoms::Array{AbstractString}`: List of (pseudo)atoms
 - `pos::Array{Float64,2}`: A matrix of cartesian coordinates characterizing the position of the atoms in the molecule, stored column-wise so that x[:, i] is the coordinate for atom `atoms[i]`.
 - `charges::Array{Float64}`: An array of charges for each specific atom.
 """
 type Molecule
 	n_atoms::Int64
-	atoms::Array{String}
+	atoms::Array{AbstractString}
 	pos::Array{Float64, 2}
 	charges::Array{Float64}
 end
@@ -22,8 +22,8 @@ end
 Reads a .mol file and gathers the relevant information to construct a Molecule.
 # TODO describe the format of the input file
 """
-function read_molecule_file(mol_filename::String)
-	extension = split(filename, ".")[end]
+function read_molecule_file(mol_filename::AbstractString)
+	extension = split(mol_filename, ".")[end]
 	if ! (extension in ["mol"])
 		error("PorousMaterials.jl can only read .mol molecule files.")
 	end
@@ -34,7 +34,7 @@ function read_molecule_file(mol_filename::String)
 
 	nAtoms = 0
 	pos = Array{Float64,2}(0,0)
-	Atoms = Array{String}(0)
+	Atoms = Array{AbstractString}(0)
 	Charge = Array{Float64}(0)
 
 	for (i, line) in enumerate(lines)
@@ -42,7 +42,7 @@ function read_molecule_file(mol_filename::String)
 			vals = split(line)
 			nAtoms = parse(Int64, vals[1])
 			pos = Array{Float64,2}(3, nAtoms)
-			Atoms = Array{String}(nAtoms)
+			Atoms = Array{AbstractString}(nAtoms)
 			Charge = Array{Float64}(nAtoms)
 		elseif (i > 4 && i < 5 + nAtoms)
 			vals = split(line)
@@ -60,7 +60,7 @@ end
 
 Converts the .mol charge convention into real charges. Grabs a string and returns an integer value
 """
-function readcharge(val::String)
+function readcharge(val::AbstractString)
 	num = parse(Int64,val)
 	if (num == 7)
 		return -3
