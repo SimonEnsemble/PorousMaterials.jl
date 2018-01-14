8b22dc0663e115d5d649862cf0d57c73b36198d8
ahyork <33434154+ahyork@users.noreply.github.com>
2018-01-14 14:44:31 -0800

    Add skeleton code for insert, delete, and move
    Still need to add guest-guest interactions to add to the get_energy function so the rest of the code can run

--- src/GCMC.jl
+++ src/GCMC.jl
@@ -3,24 +3,48 @@ using PorousMaterials
 
 export gcmc_sim
 
-function insert(molecule_pos::Array{Vector}, framework::framework, repfactor::Tuple)
-    base_energy = 
+"""
+    insert(molecule_pos::Array, framework::framework, repfactor::Tuple)
+
+Inserts an additional molecule into the system and then checks the metropolis
+hastings acceptance rules to find whether the proposed insertion is accepted or rejected.
+"""
+function insert(molecule_pos::Array{Tuple{Float64,Float64,Float64}}, framework::framework, repfactor::Tuple{Int64,Int64,Int64})
+    proposal = [molecule_pos[i] for i = 1:length(molecule_pos)]
+    push!(proposal, (rand()*repfactor[1],rand()*repfactor[2],rand()*repfactor[3]))
+    base_energy = get_energy(molecule_pos, framework)
+    prop_energy = get_energy(proposal, framework)
+    #Metropolis Hastings Acceptance rules for inserting a random particle
 end
 
-function delete()
-
+#TODO add guest-guest interactions, using this as a baseline to make sure it works
+function delete(molecule_pos::Array{Tuple{Float64,Float64,Float64}}, framework::framework, repfactor::Tuple{Int64,Int64,Int64})
+    proposal = [molecule_pos[i] for i = 1:length(molecule_pos)]
+    deleteat!(proposal,rand(1:length(proposal)))
+    base_energy = get_energy(molecule_pos, framework)
+    prop_energy = get_energy(proposal, framework)
+    #Metropolis Hastings Acceptance rules for deleting a random particle
 end
 
-function move()
-
+function move(molecule_pos::Array{Tuple{Float64,Float64,Float64}}, framework::framework, repfactor::Tuple{Int64,Int64,Int64})
+    proposal = [molecule_pos[i] for i = 1:length(molecule_pos)]
+    #Arbitray value for delta for now, can change or make more complex later
+    δ = 0.1
+    move_spot = rand(1:length(proposal))
+    proposal[move_spot] = (δ*(proposal[move_spot])[1], δ*(proposal[move_spot])[2], δ*(proposal[move_spot])[3])
+    base_energy = get_energy(molecule_pos, framework)
+    prop_energy = get_energy(proposal, framework)
+    #Metropolis Hastings Acceptance rules for moving a random particle
 end
 
-function get_N()
-
+function get_N(molecule_pos)
+    return length(molecule_pos)
 end
 
-function get_energy()
-
+function get_energy(molecule_pos::Array(Tuple), framework::framework)
+    #TODO Use vdw_energy to find energy initially
+    #TODO Develop guest-guest interactions equation
+    #TODO find a way to get ljforcefield quickly, get initial input from higher function?
 end
 
 function sim_for_N_equilibrium()
