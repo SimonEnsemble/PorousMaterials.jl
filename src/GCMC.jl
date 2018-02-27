f469918eebf3f72018e8959d37a532daf5bf4c63
ahyork <33434154+ahyork@users.noreply.github.com>
2018-02-26 19:40:42 -0800

    gcmc sim

--- src/GCMC.jl
+++ src/GCMC.jl
@@ -1,17 +1,30 @@
-module GCMC
-
-#TODO how to include other porous materials files
-
-include("Crystal.jl")
-include("Energetics_Util.jl")
-include("Energetics.jl")
-include("Molecule.jl")
-include("Forcefield.jl")
-
-export gcmc_sim
-
 #Arbitrary value for delta for now, can change or make more complex later
 const δ = 0.1
+
+
+type GCMCstats
+    n::Int
+    n²::Int
+    U_gh::Float64
+    U_gh²::Float64
+    U_gg::Float64
+    U_gg²::Float64
+    U_ggU_gh::Float64
+    num_samples::Int
+end
+
+type MarkovCounts
+    num_moves::Int
+
+    insert_proposal::Int
+    insert_acceptance::Int
+
+    translate_proposal::Int
+    translate_acceptance::Int
+
+    delete_proposal::Int
+    delete_acceptance::Int
+end
 
 """
     insert_molecule!(molecules, simulation_box, adsorbate)
@@ -36,9 +49,7 @@ molecule_id decides which molecule will be deleted, for a simulation, it must
     be a randomly generated value
 """
 function delete_molecule!(molecule_id::Int, molecules::Array{Molecule})
-    deleted_molecule = molecules[molecule_id]
     deleteat!(molecules, molecule_id)
-    return deleted_molecule
 end
 
 """
@@ -76,11 +87,12 @@ end
 Calculates energy of a single adsorbate in the system. This can be used to find
 the change in energy after an accepted proposal
 
-Code copied from Arni's Energetics.jl with minor adjustments to calculate interactions
-between the adsorbates as well as the framework
+Code copied from Arni's Energetics.jl with minor adjustments to calculate
+    interactions between the adsorbates as well as the framework
 """
 function guest_guest_vdw_energy(molecule_id::Int, molecules::Array{Molecule},
-                        ljforcefield::LennardJonesForceField, simulation_box::Box)
+                        ljforcefield::LennardJonesForceField,
+                        simulation_box::Box)
     #start energy at 0 and add to it as comparisons are made
     energy = 0.0
     #Loop over all atoms in the given molecule
@@ -129,20 +141,22 @@ runs a monte carlo simulation using the given framework and adsorbate.
 runs at the given temperature and pressure
 """
 #will pass in molecules::Array{Molecule} later
-function gcmc_sim(framework::Framework,
-                        temperature::Float64, pressure::Float64,
-                        adsorbate::String, ljforcefield::LennardJonesForceField;
-                        number_mc_trials::Int64=100000)
+function gcmc_sim(framework::Framework, temperature::Float64, pressure::Float64,
+                        adsorbate::String, ljforcefield::LennardJonesForceField,
+                        fugacity::Float64; num_mc_trials::Int=100000,
+                        num_burn_cycles::Int=10000, sample_frequency::Int=25)
+
+    # Boltmann constant (Pa-m3/K --> Pa-A3/K)
+     # const KB = 1.38064852e-23 * 1e30
+    const KB = 1.38064852e7
     const repfactors = replication_factors(framework.box, ljforcefield)
     const simulation_box = replicate_box(framework.box, repfactors)
 
-    #used to calculate average energy of the system
-    total_energy = 0.0
-    #used to add accurate values to total_energy
-    current_energy = 0.0
-    #used to calculate average number of molecules
-    total_n = 0.0
-    #start with 0 molecules
+    current_energy_gg = 0.0
+    current_energy_gh = 0.0
+    gcmc_stats = GCMCstats(0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0)
+    markov_counts = MarkovCounts([0 for i = 0:7]...)
+
     molecules = Molecule[]
 """
 #will need this later for when starting with a list of molecules
@@ -154,71 +168,124 @@ function gcmc_sim(framework::Framework,
     end
 """
 
-    for t = 1:number_mc_trials
+    for t = 1:num_mc_trials
+        markov_counts.num_moves += 1
         proposal_id = rand(1:3)
         if proposal_id == 1 #insert
+            markov_counts.insert_proposal += 1
             insert_molecule!(molecules, framework.box)
             U_gg = guest_guest_vdw_energy(length(molecules), molecules,
                 ljforcefield, simulation_box)
             U_gh = vdw_energy(framework, molecules[end],
                 ljforcefield, repfactors)
-            if rand() < exp(-(U_gg + U_gh) / temperature)
+            #Metropolis Hastings Acceptance for Insertion
+            if rand() < fugacity * sim_box.Ω / (length(molecules) * KB *
+                    temperature) * exp(-(U_gh + U_gg) / temperature)
                 #accept the move, adjust current_energy
-                current_energy += U_gg + U_gh
+                markov_counts.insert_acceptance += 1
+                current_energy_gg += U_gg
+                current_energy_gh += U_gh
             else
                 #reject the move, remove the added molecule
                 pop!(molecules)
             end
         elseif proposal_id == 2 #delete
+            markov_counts.delete_proposal += 1
             molecule_id = rand(1:length(molecules))
             U_gg = guest_guest_vdw_energy(molecule_id, molecules, ljforcefield,
                 simulation_box)
             U_gh = vdw_energy(framework, molecules[molecule_id], ljforcefield,
                 repfactors)
-            if rand() < exp((U_gg + U_gh) / temperature)
+            #Metropolis Hastings Acceptance for Deletion
+            if rand() < fugacity * sim_box.Ω / (length(molecules) * KB *
+                    temperature) * exp((U_gh + U_gg) / temperature)
                 #accept the deletion, delete molecule, adjust current_energy
+                markov_counts.delete_acceptance += 1
                 delete_molecule(molecule_id, molecules)
-                current_energy += U_gg + U_gh
+                current_energy_gg += U_gg
+                current_energy_gh += U_gh
             end
 
-        else #move
+        else #translate
+            markov_counts.translate_proposal += 1
             molecule_id = rand(1:length(molecules))
-            #energy of the molecule before it was moved
+            #energy of the molecule before it was translated
             U_gg_old = guest_guest_vdw_energy(molecule_id, molecules,
                 ljforcefield, simulation_box)
             U_gh_old = vdw_energy(framework, molecules[molecule_id],
                 ljforcefield, repfactors)
 
-            old_molecule = translate_molecule(molecule_id, molecules,
+            old_molecule = translate_molecule!(molecule_id, molecules,
                 simulation_box)
 
-            #energy of the molecule after it is moved
+            #energy of the molecule after it is translated
             U_gg_new = guest_guest_vdw_energy(molecule_id, molecules,
                 ljforcefield, simulation_box)
             U_gh_new = vdw_energy(framework, molecules[molecule_id],
                 ljforcefield, rep_factors)
 
+            #Metropolis Hastings Acceptance for translation
             if rand() < exp(-((U_gg_new + U_gh_new) - (U_gg_old + U_gh_old))
                 / temperature)
                 #accept the move, adjust current energy
-                #remove energy from old molecule coords
-                current_energy -= U_gg_old + U_gh_old
-                #add energy from new moleculde coords
-                current_energy += U_gg_new + U_gh_new
+                markov_counts.translate_acceptance += 1
+                current_energy_gg += U_gg_new - U_gg_old
+                current_energy_gh += U_gh_new - U_gh_old
             else
                 #reject the move, reset the molecule at molecule_id
                 molecules[molecule_id] = old_molecule
             end
 
+        end #which move the code executes
+        #sample the current state space
+        if(t > num_burn_cycles && t % sample_frequency == 0)
+            gcmc_stats.n += length(molecules)
+            gcmc_stats.n² += length(molecules) ^ 2
+            gcmc_stats.U_gh += current_energy_gh
+            gcmc_stats.U_gh² += current_energy_gh ^ 2
+            gcmc_stats.U_gg += current_energy_gg
+            gcmc_stats.U_gg² += current_energy_gg ^ 2
+            gcmc_stats.U_ggU_gh += current_energy_gg * current_energy_gh
+            gcmc_stats.num_samples += 1
         end
-        #adjust these values to calculate average values later
-        total_energy += current_energy
-        total_n += length(molecules)
-    end
 
-    average_n = total_n / number_mc_trials
-    average_energy = total_energy / number_mc_trials
+    end #finished markov chain proposal moves
+
+    results = Dict{String, Union{Int, Float64}}()
+    results["⟨N⟩ (molecules)"] = gcmc_stats.n / gcmc_stats.num_samples
+    results["⟨N⟩ (molecules/unit cell)"] = results["⟨N⟩ (molecules)"] /
+        (repfactors[1] * repfactors[2] * repfactors[3])
+    #(molecules/unit cell) * (mol/6.02 * 10^23 molecules) * (1000 mmol/mol) *
+    #    (unit cell/framework amu) * (amu/ 1.66054 * 10^-24)
+    results["⟨N⟩ (mmol/g)"] = results["⟨N⟩ (molecules/unit cell)"] * 1000 /
+        (6.022140857e23 * molecular_weight(framework) * 1.66054e-24)
+    results["⟨U_gg⟩ (K)"] = gcmc_stats.U_gg / gcmc_stats.num_samples
+    results["⟨U_gh⟩ (K)"] = gcmc_stats.U_gh / gcmc_stats.num_samples
+    results["⟨Energy⟩ (K)"] = (gcmc_stats.U_gg + gcmc_stats.U_gh) /
+        gcmc_stats.num_samples
+    #variances
+    results["var(N)"] = (gcmc_stats.n² / gcmc_stats.num_samples) -
+        (results["⟨N⟩ (molecules)"] ^ 2)
+    results["var(U_gg)"] = (gcmc_stats.U_gg² / gcmc_stats.num_samples) -
+        (results["⟨U_gg⟩ (K)"] ^ 2)
+    results["var⟨U_gh⟩"] = (gcmc_stats.U_gh² / gcmc_stats.num_samples) -
+        (results["⟨U_gh⟩ (K)"] ^ 2)
+    results["var(Energy)"] = ((gcmc_stats.U_gg² + gcmc_stats.U_gh² + 2 *
+        gcmc_stats.U_ggU_gh) / gcmc_stats.num_samples) -
+        (results["⟨Energy⟩ (K)"] ^ 2)
+    #insertions stats
+    results["Number of attempted Insertions"] = markov_counts.insert_proposal
+    results["Percentage of accepted Insertions"] =
+        markov_counts.insert_acceptance / markov_counts.insert_proposal
+    #deletion stats
+    results["Number of attempted Deletions"] = markov_counts.delete_proposal
+    results["Percentage of accepted Deletions"] =
+        markov_counts.delete_acceptance / markov_counts.delete_proposal
+    #translation stats
+    results["Number of attempted Translations"] = markov_counts.translate_proposal
+    results["Percentage of accepted Translations"] =
+        markov_counts.translate_acceptance / markov_counts.translate_proposal
+
+    return results
 
 end #gcmc_sim
-
-end
