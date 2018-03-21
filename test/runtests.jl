554e60bce17de1430bf20e8f99eb6f98dcbfec22
CorySimon <corysimon@berkeley.edu>
2018-03-20 17:33:42 -0700

    added more rigorous tests for crystals

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,24 +8,40 @@ using Base.Test
 # Run Tests
 
 @printf("------------------------------\nTesting Crystal.jl\n\n")
-frame = read_crystal_structure_file("test_structure.cif")
-strip_numbers_from_atom_labels!(frame)
+framework = read_crystal_structure_file("test_structure2.cif") # .cif
+strip_numbers_from_atom_labels!(framework)
 @testset "Crystal Tests" begin
-    @test frame.name == "test_structure.cif"
-	@test frame.box.f_to_c * frame.box.c_to_f ≈ eye(3)
-	@test frame.box.a == frame.box.b == frame.box.c == 1.0
-	@test frame.box.α == frame.box.β == frame.box.γ == 90 * (pi / 180)
-	@test frame.box.Ω ≈ 1
-    @test frame.n_atoms == 1
-	@test frame.atoms == ["Zn"]
-	@test frame.xf == [0.0, 0.0, 0.0][:, :]
-    @test frame.charges[1] ≈ 1.082578
-    @test chemical_formula(frame) == Dict("Zn" => 1)
-    @test molecular_weight(frame) ≈ 65.38
+    @test framework.name == "test_structure2.cif"
+	@test framework.box.f_to_c * framework.box.c_to_f ≈ eye(3)
+	@test framework.box.a ≈ 10.0
+	@test framework.box.b ≈ 20.0
+	@test framework.box.c ≈ 30.0
+	@test framework.box.α ≈ 90.0 * (pi / 180)
+	@test framework.box.β ≈ 45.0 * (pi / 180)
+	@test framework.box.γ ≈ 120.0 * (pi / 180)
+	@test framework.box.Ω ≈ det(framework.box.f_to_c)
+    @test framework.n_atoms == 2
+	@test framework.atoms == ["Ca", "O"]
+	@test framework.xf == [0.2 0.6; 0.5 0.3; 0.7 0.1]
+    @test framework.charges == [1.0, -1.0]
+    @test chemical_formula(framework) == Dict("Ca" => 1, "O" => 1)
+    @test molecular_weight(framework) ≈ 15.9994 + 40.078
+
+    # test .cssr reader too.
+    framework_from_cssr = read_crystal_structure_file("test_structure2.cif")
+    strip_numbers_from_atom_labels!(framework_from_cssr)
+    @test all(framework_from_cssr.xf .== framework.xf)
+    @test all(framework_from_cssr.charges .== framework.charges)
+    @test all(framework_from_cssr.atoms == framework.atoms)
+    @test framework_from_cssr.n_atoms == framework.n_atoms
+    @test all(framework_from_cssr.box.f_to_c .== framework.box.f_to_c)
+    @test all(framework_from_cssr.box.c_to_f .== framework.box.c_to_f)
 end;
 
 @printf("------------------------------\nTesting Forcefield.jl\n\n")
 const ljforcefield = read_forcefield_file("test_forcefield.csv", cutoffradius=12.5, mixing_rules="Lorentz-Berthelot")
+frame = read_crystal_structure_file("test_structure.cif") # .cif
+strip_numbers_from_atom_labels!(frame)
 rep_factors = replication_factors(frame.box, ljforcefield)
 @testset "Forcefield Tests" begin
 	@test ljforcefield.pure_sigmas["He"] == 1.0
@@ -46,6 +62,7 @@ energy1 = vdw_energy(frame2, molecule3, ljforcefield, rep_factors2)
 molecule3.x[1] = 0.494265; molecule3.x[2] = 2.22668; molecule3.x[3] = 0.450354;
 energy2 = vdw_energy(frame2, molecule3, ljforcefield, rep_factors2)
 @testset "Energetics Tests" begin
+	@test frame.box.Ω ≈ 1
 	@test vdw_energy(frame, molecule1, ljforcefield, rep_factors) ≈ vdw_energy(frame, molecule2, ljforcefield, rep_factors)
 	@test vdw_energy(frame, molecule1, ljforcefield, (1,1,1)) ≈ 4 * ljforcefield.epsilons["He"]["Zn"] * ((ljforcefield.sigmas_squared["Zn"]["He"] / 0.75) ^ 6 - (ljforcefield.sigmas_squared["Zn"]["He"] / 0.75) ^ 3 )
 	@test isapprox(energy1, -5041.58, atol = 0.005) # comparing to RASPA
@@ -57,26 +74,6 @@ end;
 #
 #GCMC Tests
 #
-
-"""
-
-    use_nearest_image = completely_outside_box(molecule, sim_box)
-
-returns true if the given molecule is completely outside of a given box
-"""
-function completely_outside_box(molecule::Molecule, box::Box)
-    xf = box.c_to_f * molecule.x
-    for coords = 1:3
-        #if none of the coords are less than 1 it must be outside of the box
-        if sum(xf[coords, :] .<= 1.0) == 0
-            return true
-        #if none of the coords are greater than 0 it must be outside of the box
-        elseif sum(xf[coords, :] .>= 0.0) == 0
-            return true
-        end
-    end
-    return false
-end
 
 #
 #INSERTION TESTS
