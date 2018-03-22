7eb39a4b8b3f0953d0e5efec313bde1dae69599f
CorySimon <corysimon@berkeley.edu>
2018-03-22 11:21:51 -0700

    gcmc tests run but not correct

--- test/gcmc_test.jl
+++ test/gcmc_test.jl
@@ -10,7 +10,7 @@ test_mmol_g = [0.1904, 1.007, 1.4007]
 test_molec_unit_cell = [0.262, 1.388, 1.929]
 
 for (i, fugacity) in enumerate(test_fugacities)
-    results = gcmc_simulation(sbmof1, 298.0, fugacity, "Xe", dreiding_forcefield, n_burn_cycles=5000, n_sample_cycles=5000, verbose=true)
-    isapprox(results["⟨N⟩ (molecules/unit cell)"], test_molec_unit_cell[i], rtol=0.005)
-    isapprox(results["⟨N⟩ (mmol/g)"], test_mmol_g[i], rtol=0.005)
+    @time results = gcmc_simulation(sbmof1, 298.0, fugacity, "Xe", dreiding_forcefield, n_burn_cycles=5000, n_sample_cycles=5000, verbose=true)
+ #     isapprox(results["⟨N⟩ (molecules/unit cell)"], test_molec_unit_cell[i], rtol=0.005)
+ #     isapprox(results["⟨N⟩ (mmol/g)"], test_mmol_g[i], rtol=0.005)
 end
