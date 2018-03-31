d3d3522cb425866884c7da7536741a4e053e9b82
CorySimon <CorySimon@berkeley.edu>
2018-03-31 11:10:01 -0700

    added molecule reader

--- src/Molecules.jl
+++ src/Molecules.jl
@@ -1,6 +1,5 @@
 # Molecules are composed of Lennard-Jones spheres and point charges.
-"A Lennard-Jones sphere"
-struct LJsphere
+struct LennardJonesSphere
     "atom name corresponding to force field"
     atom::Symbol
     "Cartesian coordinates (units: A)"
@@ -8,7 +7,7 @@ struct LJsphere
 end
 
 "A point charge"
-struct Charge
+struct PointCharge
     "charge magnitude (electrons)"
     q::Float64
     "Cartesian coordinates (units: A)"
@@ -20,13 +19,13 @@ Data structure for a molecule/adsorbate.
 
 # Attributes
 - `species::Symbol`: Species of molecule, e.g. `CO2` or `ethane`
-- `ljspheres::Array{LJspheres, 1}`: array of Lennard-Jones spheres comprising the molecule
-- `charges::Array{Charges, 1}`: array of point charges comprising the molecule
+- `ljspheres::Array{LennardJonesSphere, 1}`: array of Lennard-Jones spheres comprising the molecule
+- `charges::Array{PointCharges, 1}`: array of point charges comprising the molecule
 """
 struct Molecule
     species::Symbol
-    ljspheres::Array{LJsphere, 1}
-    charges::Array{Charge, 1}
+    ljspheres::Array{LennardJonesSphere, 1}
+    charges::Array{PointCharge, 1}
 end
 
 function Base.show(io::IO, molecule::Molecule)
@@ -45,4 +44,46 @@ function Base.show(io::IO, molecule::Molecule)
                     charge.x[1], charge.x[2], charge.x[3])
         end
     end
+end
+
+"""
+    molecule = read_molecule_file("Xe")
+
+Reads molecule files in the directory `PorousMaterials.PATH_TO_DATA * "/molecule/" * species * "/"`.
+"""
+function read_molecule_file(species::String)
+    if ! isdir(PATH_TO_DATA * "molecules/" * species)
+        error(@sprintf("No directory created for %s in %s\n", species,
+                       PATH_TO_DATA * "molecules/"))
+    end
+    
+    # Read in Lennard Jones spheres
+    ljspheresfilename = PATH_TO_DATA * "molecules/" * species * "/lennard_jones_spheres.csv"
+    if ! isfile(ljspheresfilename)
+        error(@sprintf("No file %s exists. Even if there are no Lennard Jones spheres in your molecule, include a .csv file with the proper headers but no rows.",
+                       ljspheresfilename))
+    end
+    df_lj = CSV.read(ljspheresfilename)
+    
+    ljspheres = LennardJonesSphere[]
+    for row in eachrow(df_lj)
+        x = [row[:x], row[:y], row[:z]]
+        push!(ljspheres, LennardJonesSphere(Symbol(row[:atom]), x))
+    end
+
+    # Read in point charges
+    chargesfilename = PATH_TO_DATA * "molecules/" * species * "/point_charges.csv"
+    if ! isfile(chargesfilename)
+        error(@sprintf("No file %s exists. Even if there are no point charges in your molecule, include a .csv file with the proper headers but no rows.",
+                       chargesfilename))
+    end
+    df_c = CSV.read(chargesfilename)
+
+    charges = PointCharge[]
+    for row in eachrow(df_c)
+        x = [row[:x], row[:y], row[:z]]
+        push!(ljspheres, Charge(row[:q], x))
+    end
+    
+    return Molecule(Symbol(species), ljspheres, charges)
 end
