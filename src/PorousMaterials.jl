749990fcb070193c5c83d3cba1959bba9c783596
CorySimon <corysimon@berkeley.edu>
2018-04-05 11:20:55 -0700

    add outside_box name change

--- src/PorousMaterials.jl
+++ src/PorousMaterials.jl
@@ -20,7 +20,7 @@ export Box, Framework, read_crystal_structure_file, replicate_to_xyz,
        LennardJonesForceField, read_forcefield_file, replication_factors, check_forcefield_coverage, # Forcefield.jl
        Molecule, read_molecule_file, translate_by!, translate_to!, rotate!, # Molecules.jl
        lennard_jones, vdw_energy, # Energetics.jl
-       completely_outside_box,
+       outside_box,
        read_xyz, read_cpk_colors, read_atomic_masses, read_atomic_radii, # Misc.jl
 	Grid, write_to_cube, # Grid.jl
 	Snapshot, snap, write_snapshot_to_vtk, write_to_npy, write_snapshot_to_cube # Snapshot.jl #TODO Remove this line later
