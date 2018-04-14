6870e8afdccb8372a04ed31f0622d0ada0a14ffb
CorySimon <corysimon@berkeley.edu>
2018-04-14 12:37:15 -0700

    add performance annotations; add Ewald tests to runtests

--- src/Energetics_Util.jl
+++ src/Energetics_Util.jl
@@ -9,7 +9,7 @@ See comments in vdw_energy for more description.
 function nearest_image!(dxf::Array{Float64, 1}, repfactors::Tuple{Int, Int, Int})
     for k = 1:3 # loop over components
         if abs(dxf[k]) > repfactors[k] / 2.0
-            dxf[k] -= sign(dxf[k]) * repfactors[k]
+            @inbounds dxf[k] -= sign(dxf[k]) * repfactors[k]
         end
     end
 end
@@ -18,7 +18,7 @@ function nearest_image!(dxf::Array{Float64, 2}, repfactors::Tuple{Int, Int, Int}
     for a = 1:size(dxf)[2] # loop over atoms
         for k = 1:3 # loop over components
             if abs(dxf[k, a]) > repfactors[k] / 2.0
-                dxf[k, a] -= sign(dxf[k, a]) * repfactors[k]
+                @inbounds dxf[k, a] -= sign(dxf[k, a]) * repfactors[k]
             end
         end
     end
--- test/runtests.jl
+++ test/runtests.jl
@@ -174,8 +174,34 @@ end
     energy6 = vdw_energy(sbmof1, xenon, ljforcefield, rep_factors_sbmof1)
 	@test isapprox(energy1, energy6, atol = 0.00001)
 end;
+@printf("------------------------------\n")
 
 
+@printf("------------------------------\nTesting Ewald.jl\n\n")
+framework = read_crystal_structure_file("NU-1000_Greg.cif")
+
+k_rep_factors = (11, 11, 9)
+α = 0.265058
+sr_cutoff = 12.5
+rep_factors = replication_factors(framework, sr_cutoff)
+sim_box = replicate_box(framework.box, rep_factors)
+const kvectors = compute_kvectors(sim_box, k_rep_factors, α)
+
+q_test = 0.8096
+
+@testset "Ewald summation Tests" begin
+    x = [9.535619863743, 20.685576379935, 0.127344239990]
+    ϕ = electrostatic_potential(framework, x, sim_box, rep_factors, sr_cutoff, kvectors, α)
+    @test isapprox(ϕ * q_test, 111373.38, atol=2.5)
+
+    x = [4.269654927228, 23.137319129548, 28.352847101096]
+    ϕ = electrostatic_potential(framework, x, sim_box, rep_factors, sr_cutoff, kvectors, α)
+    @test isapprox(ϕ * q_test, -531.0, atol=0.5)
+
+    x = [-0.047382031804, 7.209555961450, 5.158180463556]
+    ϕ = electrostatic_potential(framework, x, sim_box, rep_factors, sr_cutoff, kvectors, α)
+    @test isapprox(ϕ * q_test, -2676.8230141, atol=0.5)
+end
 @printf("------------------------------\n")
 
 @printf("------------------------------\nTesting GCMC.jl\n\n")
