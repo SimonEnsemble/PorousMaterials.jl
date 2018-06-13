75f9dfa7f5be89d64e7c43b2ed6ada382b63f626
CorySimon <corysimon@berkeley.edu>
2018-06-13 10:04:07 -0700

    add reinsert molecule

--- src/PorousMaterials.jl
+++ src/PorousMaterials.jl
@@ -24,7 +24,7 @@ export Box, Framework, read_crystal_structure_file, replicate_to_xyz, remove_ove
        lennard_jones, vdw_energy, vdw_energy_no_PBC, # VdWEnergetics.jl
        read_xyz, read_cpk_colors, read_atomic_radii, # Misc.jl
        Grid, write_cube, read_cube, energy_grid, # Grid.jl
-       insert_molecule!, delete_molecule!, translate_molecule!, # MChelpers.jl
+       insert_molecule!, delete_molecule!, translate_molecule!, reinsert_molecule!, # MChelpers.jl
        apply_periodic_boundary_condition!,
        guest_guest_vdw_energy, gcmc_simulation, adsorption_isotherm, stepwise_adsorption_isotherm,  # GCMC.jl
        electrostatic_potential, electrostatic_potential_energy, precompute_kvec_wts, allocate_eikr, setup_Ewald_sum # ElectrostaticEnergetics.jl
