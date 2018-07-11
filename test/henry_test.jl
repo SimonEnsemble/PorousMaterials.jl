13a520d81b8668759cf1c8e94b43608f18bba65f
CorySimon <corysimon@berkeley.edu>
2018-07-11 12:19:53 -0700

    returns a dictionary of results now; progress meter added; do not increment to energy sum when Inf; test added

--- test/henry_test.jl
+++ test/henry_test.jl
@@ -1,7 +1,11 @@
 using PorousMaterials
-molecule = read_molecule_file("CO2")
+using Base.Test
+    
 framework = read_crystal_structure_file("SBMOF-1.cif")
-ljff = read_forcefield_file("UFF.csv")
+ljff = read_forcefield_file("Dreiding.csv", cutoffradius=12.5)
+molecule = read_molecule_file("Xe")
 temperature = 298.0
 
-henry_coeff = henry_coefficient(framework, molecule, temperature, ljff, nb_insertions=100)
+result = henry_coefficient(framework, molecule, temperature, ljff, nb_insertions=10000000, verbose=true)
+@test isapprox(result["henry coefficient [mol/(kg-Pa)]"], 0.00985348, atol=0.000135146)
+, # 0.00985348 +/- 0.000135146 mol/kg/Pa
