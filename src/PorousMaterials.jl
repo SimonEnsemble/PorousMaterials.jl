d48d29d34a3ba1a0e9362780590a081c1b9afd5c
CorySimon <corysimon@berkeley.edu>
2018-07-17 13:49:16 -0700

    captialize name of file

--- src/PorousMaterials.jl
+++ src/PorousMaterials.jl
@@ -13,7 +13,7 @@ include("Misc.jl")
 include("Grid.jl")
 include("MChelpers.jl")
 include("EOS.jl")
-include("henry.jl")
+include("Henry.jl")
 include("GCMC.jl")
 
 export Box, Framework, read_crystal_structure_file, replicate_to_xyz, remove_overlapping_atoms,
@@ -32,5 +32,5 @@ export Box, Framework, read_crystal_structure_file, replicate_to_xyz, remove_ove
        gcmc_simulation, adsorption_isotherm, stepwise_adsorption_isotherm,  # GCMC.jl
        electrostatic_potential, electrostatic_potential_energy, precompute_kvec_wts, setup_Ewald_sum, total, # ElectrostaticEnergetics.jl
        calculate_properties, PengRobinsonGas, # EOS.jl
-       henry_coefficient # henry.jl
+       henry_coefficient # Henry.jl
 end
