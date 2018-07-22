b92476fe49f4a15d4ed85ee5c7320e227f597110
Cory Simon <simoncor@submit-em64t-03.hpc.engr.oregonstate.edu>
2018-07-22 13:02:55 -0700

    HT faciltiies working

---
+++ high_throughput_helpers/collect_results.jl
@@ -0,0 +1,27 @@
+using PorousMaterials
+using JLD
+ # using PyPlot
+
+include("jobs_to_run.jl")
+
+for (structure, conditions) in jobs_to_run
+    ljff = jobs_to_run[structure]["forcefield"]
+    n_burn_cycles = jobs_to_run[structure]["n_burn_cycles"]
+    n_sample_cycles = jobs_to_run[structure]["n_sample_cycles"]
+
+    for gas in conditions["gases"]
+        for temperature in conditions["temperatures"]
+            # store results per adsorption isotherm
+            results = []
+            for pressure in conditions["pressures"]
+
+                save_results_filename = PorousMaterials.PATH_TO_DATA * "gcmc_sims/" * PorousMaterials.root_save_filename(
+                    structure, Symbol(gas), ljff.name, temperature, pressure, 
+                    n_burn_cycles, n_sample_cycles) * ".jld"
+
+                result = JLD.load(save_results_filename)["results"]
+                push!(results, result)
+            end
+        end
+    end
+end
--- high_throughput_helpers/run_gcmc_simulation.jl
+++ high_throughput_helpers/run_gcmc_simulation.jl
@@ -1,9 +1,6 @@
 # example use, e.g. to run μVT sim of CH4 in CAXVII_clean.cif at 1 bar, 298 K:
 #   julia run_gcmc_simulation.jl CAXVII_clean CH4 298.0 1.0
 
-using PorousMaterials
-using JLD
-
 # read in command line arguments
 if length(ARGS) != 4
     error("Run as: julia run_gcmc_simulation.jl structure gas temperature(K) pressure(bar)")
@@ -14,10 +11,12 @@ gasname = ARGS[2]
 temperature = parse(Float64, ARGS[3])
 pressure = parse(Float64, ARGS[4])
 
+include("jobs_to_run.jl") # contains ljff and # samples
+
 # simulation set-up
-ff = LJForceField("Greg_bogus_ZIF71.csv", cutoffradius=12.8)
-n_burn_cycles = 5000
-n_sample_cycles = 5000
+ljff = jobs_to_run[structurename]["forcefield"]
+n_burn_cycles = jobs_to_run[structurename]["n_burn_cycles"]
+n_sample_cycles = jobs_to_run[structurename]["n_sample_cycles"]
 
 xtal = Framework(structurename * ".cif")
 strip_numbers_from_atom_labels!(xtal)
