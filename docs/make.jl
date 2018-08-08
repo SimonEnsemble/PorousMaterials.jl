edd63400959661cc89ea934fcf927e89007066a7
Arthur York <yorkar@oregonstate.edu>
2018-08-07 18:19:37 -0700

    added files to start making a documenation site using Documenter.jl

---
+++ docs/make.jl
@@ -0,0 +1,19 @@
+using Documenter, PorousMaterials
+
+    makedocs(
+        modules = [PorousMaterials],
+        sitename = "PorousMaterials.jl",
+        pages = [
+                "Home" => "index.md"
+                "Functions" => "functions.md"
+                # Any other pages go here
+                ]
+    )
+
+    deploydocs(
+        repo = "https://github.com/SimonEnsemble/PorousMaterials.jl",
+        # This is a link to the main repo and the master branch
+        julia = "0.6",
+        osname = "linux",
+        deps = nothing # These are dependencies for the site, not the package
+    )
