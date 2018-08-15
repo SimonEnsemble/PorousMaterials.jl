86e7f82ae662653e38544bd4455ad2511d249508
Arni Sturluson <sturlusa@oregonstate.edu>
2018-08-15 12:48:45 -0700

    Add checkpoint test

--- test/runtests.jl
+++ test/runtests.jl
@@ -836,3 +836,39 @@ end
     @test isapprox(props["density (mol/m³)"], 3000.054418, atol=0.2)
     @test isapprox(props["molar volume (L/mol)"], 0.333327, atol=1e-4)
 end
+
+@testset "GCMC Checkpoint Tests" begin
+    framework = Framework("SBMOF-1.cif")
+    co2 = Molecule("CO2")
+    co_bond_length = norm(co2.atom[1].xf - co2.atom[2].xf)
+    ljff = LJForceField("UFF.csv")
+    temp = 298.0
+    pressures = 0.5
+    molecules = Array{Molecule, 1}
+
+    results = Dict()
+    srand(1234)
+    for i = 1:3
+        if i == 1
+            checkpoint = Dict()
+        else
+            checkpoint = JLD.load(PorousMaterials.PATH_TO_DATA * "/gcmc_checkpoints/" * gcmc_results_savename(framework.name, co2.species, ljff.name, temp, pressure, 5, 5 * i, "_checkpoint"), "checkpoint")
+        end
+        results, molecules = gcmc_simulation(frame, deepcopy(co2), temp, pressure, ljff,
+                                             n_burn_cycles=5, n_sample_cycles=5 * (i + 1),
+                                             verbose=true, sample_frequency=1, eos=:PengRobinson,
+                                             autosave=false, write_checkpoints=true, 
+                                             checkpoint=checkpoint, checkpoint_frequency=1)
+        @test isapprox(norm(molecules[1].atoms[1].xf - molecules[1].atoms[2].xf), co_bond_length)
+    end
+
+    srand(1234)
+    results2, molecules2 = gcmc_simulation(frame, deepcopy(co2), temp, pressure, ljff,
+                                         n_burn_cycles=5, n_sample_cycles=20,
+                                         verbose=true, sample_frequency=1, eos=:PengRobinson,
+                                         autosave=false, write_checkpoints=false, 
+                                         load_checkpoint_file=false, checkpoint_frequency=1)
+    @test isapprox(norm(molecules2[1].atoms[1].xf - molecules2[1].atoms[2].xf), co_bond_length)
+    @test all(isapprox.(molecules, molecules2))
+    @test isapprox(results["Q_st (K)"], results2["Q_st (K)"])
+end
