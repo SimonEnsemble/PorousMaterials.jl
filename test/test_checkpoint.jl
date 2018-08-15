82937d911c4a733e8d0c83ecda7b52f7dc6b6753
CorySimon <CorySimon@berkeley.edu>
2018-08-15 09:51:51 -0700

    hmm

--- test/test_checkpoint.jl
+++ test/test_checkpoint.jl
@@ -13,26 +13,30 @@ co_bond_length = norm(co2.atoms[1].xf - co2.atoms[2].xf)
 ljff = LJForceField("UFF.csv")
 temp = 298.0
 pressure = 0.5
+molecules = Molecule[]
 
 results = Dict()
 srand(1234)
+println(" a random no: ", rand())
 
 for i = 1:3
     @printf("i = %d\n",i)
-    checkpoint_file = i == 1 ? false : gcmc_result_savename(frame.name, co2.species, ljff.name, temp, pressure, 5, 5*(i-1), "_checkpoint")
+    checkpoint_file = i == 1 ? false : gcmc_result_savename(frame.name, co2.species, ljff.name, temp, pressure, 5, 5 * i, "_checkpoint")
     results, molecules = gcmc_simulation(frame, co2, temp, pressure, ljff,
-                                         n_burn_cycles = 5, n_sample_cycles=5*i,
+                                         n_burn_cycles=5, n_sample_cycles=5 *(i + 1),
                                          verbose=true, sample_frequency=1, eos=:PengRobinson,
                                          autosave=false, write_checkpoints=true, load_checkpoint=checkpoint_file, checkpoint_frequency=1)
-    @test isapprox(norm(molecules[1].atoms[1].xf - molecules[1].atoms[2].xf), co_bond_length)
+    @assert isapprox(norm(molecules[1].atoms[1].xf - molecules[1].atoms[2].xf), co_bond_length)
 end
 
 println("---------------------------------")
 
 srand(1234)
+println(" a random no: ", rand())
 
 results2, molecules2 = gcmc_simulation(frame, co2, temp, pressure, ljff,
-                          n_burn_cycles=5, n_sample_cycles = 15,
+                          n_burn_cycles=5, n_sample_cycles=20,
                           verbose=true, sample_frequency=1, eos=:PengRobinson,
                           autosave=false, write_checkpoints=false, load_checkpoint=false, checkpoint_frequency=1, progressbar=true)
+@assert isapprox(norm(molecules2[1].atoms[1].xf - molecules2[1].atoms[2].xf), co_bond_length)
  # @test all(isapprox.(molecules, molecules2))
