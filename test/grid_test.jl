3a97e6e0cde934e745e7e7daa48ada2e5a52f1c6
CorySimon <corysimon@berkeley.edu>
2018-09-12 17:26:07 -0700

    add energy grid test

--- test/grid_test.jl
+++ test/grid_test.jl
@@ -9,10 +9,16 @@ using Statistics
 using Random
 
 @testset "Grid Tests" begin
+    # test read and write
     grid = Grid(Box(0.7, 0.8, 0.9, 1.5, 1.6, 1.7), (3, 3, 3), rand(Float64, (3, 3, 3)),
         :kJ_mol, [1., 2., 3.])
     write_cube(grid, "test_grid.cube")
     grid2 = read_cube("test_grid.cube")
     @test isapprox(grid, grid2)
+    
+    framework = Framework("LTA.cif")
+    molecule = Molecule("Xe")
+    forcefield = LJForceField("UFF.csv")
+    grid = energy_grid(framework, molecule, forcefield, n_pts=(10, 10, 10))
 end
 end
