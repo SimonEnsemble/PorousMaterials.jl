702da53730abc9a640ed2481ed0b2d9d397ae5c8
Andrew S. Rosen <rosen@u.northwestern.edu>
2018-09-14 15:37:19 -0500

    Update path_test.jl
    Don't include ` set_path_to_data(pwd())` and `set_path_to_data()` in `joinpath`

--- test/path_test.jl
+++ test/path_test.jl
@@ -10,8 +10,10 @@ using Random
 
 @testset "Path Tests" begin
     set_tutorial_mode()
-    @test PorousMaterials.PATH_TO_DATA == joinpath(dirname(pathof(PorousMaterials)),"../test/data/",set_path_to_data(pwd()))
-    @test PorousMaterials.PATH_TO_DATA == joinpath(pwd(),set_path_to_data())
+    @test PorousMaterials.PATH_TO_DATA == joinpath(dirname(pathof(PorousMaterials)),"../test/data/")
+    set_path_to_data(pwd())
+    @test PorousMaterials.PATH_TO_DATA == pwd()
+    set_path_to_data()
     @test PorousMaterials.PATH_TO_DATA == joinpath(pwd(),"data")
 end
 end
