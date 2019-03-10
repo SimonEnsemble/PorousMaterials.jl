4b403e0b470a6a75e63a33bca2692e05e0018431
Arni Sturluson <sturlusa@oregonstate.edu>
2019-03-10 11:51:18 -0700

    Removed old functions and added docstring to

--- src/Misc.jl
+++ src/Misc.jl
@@ -137,86 +137,6 @@ function read_atomic_masses()
     return atomic_masses
 end
 
-"""
-    henry, rmse = extract_henry_coefficient(df, pressure_col_name, loading_col_name, nb_pts_to_include)
-
-Extract the Henry coefficient from an isotherm contained in `df`. Will use `nb_pts_to_include` points to find the slope (which is equivalent
-to the Henry coefficient). Does not convert any units.
-
-# Arguments
-- `df::DataFrame`: A DataFrame object that contains the isotherm we wish to extract the Henry coefficient from
-- `pressure_col_name::Symbol`: The name of the pressure column. Takes in symbols due to DataFrame structure
-- `loading_col_name::Symbol`: The name of the loading/adsorption column. Takes in symbols due to DataFrame structure
-- `nb_pts_to_include::Int`: The number of points used to fit the best Henry coefficient
-
-# Returns
-- `henry::Float64`: The henry coefficient from the isotherm in `df`
-- `rmse::Float64`: The Root Mean Squared Error of the fit
-"""
-function extract_henry_coefficient(df::DataFrame, pressure_col_name::Symbol, loading_col_name::Symbol, nb_pts_to_include::Int)
-    sort!(df, [pressure_col_name])
-    P = zeros(nb_pts_to_include, 1)
-    n = zeros(nb_pts_to_include)
-    for (i, row) in enumerate(eachrow(df))
-        if i > nb_pts_to_include
-            break
-        end
-        P[i, 1] = row[pressure_col_name]
-        n[i] = row[loading_col_name]
-    end
-    henry = llsq(P, n; bias=false)[1]
-    prediction = henry * P
-    rmse = sqrt(sum(abs2.(prediction - n))/length(prediction))
-    return henry, rmse
-end
-
-"""
-    M, K = fit_langmuir(df, pressure_col_name, loading_col_name, M0=1.0, K0=1.0)
-
-Takes in a DataFrame `df` containing an isotherm. Will try to fit a Langmuir model to the data.
-The Langmuir model is in the following form:
-N = (MKP)/(1+KP)
-where N is the total adsorption, M is the maximum monolayer coverage, K is the Langmuir constant and P is the partial pressure of the gas.
-A minimization problem is solved using `M0` and `K0` as initial guesses for the maximum monolayer coverage and Langmuir constant, respectively.
-
-# Arguments
-- `df::DataFrame`: The DataFrame containing the pressure and adsorption data for the isotherm
-- `pressure_col_name::Symbol`: The header of the pressure column. Can be found with `names(df)`
-- `loading_col_name::Symbol`: The header of the loading/adsorption column. Can be found with `names(df)`
-- `M0::Float64`: An initial guess for the maximum monolayer coverage
-- `K0::Float64`: An initial guess for the Langmuir constant
-
-# Returns
-- `M::Float64`: The maximum monolayer coverage according to Langmuir's theory, N = (MKP)/(1+KP)
-- `K::Float64`: The Langmuir constant
-"""
-function fit_langmuir(df::DataFrame, pressure_col_name::Symbol, loading_col_name::Symbol, method::Symbol=:nonlinear)
-    sort!(df, [pressure_col_name])
-    if method == :nonlinear
-        n = df[loading_col_name]
-        p = df[pressure_col_name]
-        #M0 = n[end] * 1.1
-        #K0 = n[2] / (p[2] * (M0-n[2]))
-        M0, K0 = fit_langmuir(df, pressure_col_name, loading_col_name, :linear)
-        function_to_minimize(θ) = return sum([(n[i] - θ[1] * θ[2] * p[i]/(1 + θ[2] * p[i]))^2 for i = 1:length(n)])
-        θ0 = [M0, K0]
-        res = optimize(function_to_minimize, θ0, LBFGS())
-        M, K = res.minimizer
-    elseif method == :linear
-        inv_N = (df[loading_col_name]).^(-1)
-        inv_P = (df[pressure_col_name]).^(-1)
-        if inv_P[1] == Inf || inv_N[1] == Inf
-            slope, intercept = llsq(inv_P[2:end,:], inv_N[2:end])
-        else
-            slope, intercept = llsq(inv_P, inv_N)
-        end
-        M = 1/intercept
-        K = 1/(M * slope)
-    else
-        error("Method not available. Try `:linear` or `:nonlinear`")
-    end
-    return M, K
-end
 
 function _guess(df::DataFrame, pressure_col_name::Symbol, loading_col_name::Symbol, model::Symbol)
     n = df[loading_col_name]
@@ -243,6 +163,28 @@ end
 
 
 
+"""
+    params = fit_isotherm(df, pressure_col_name, loading_col_name, model)
+
+Takes in a DataFrame `df` containing an isotherm. Will try to fit a model to the data.
+Available models are `:henry` and `:langmuir`
+The Langmuir model is in the following form:
+N = (MKP)/(1+KP)
+where N is the total adsorption, M is the maximum monolayer coverage, K is the Langmuir constant and P is the partial pressure of the gas.
+`params` will contain a fit to M and K
+The Henry model takes the following form:
+N = KP
+and `params` will contain K
+
+# Arguments
+- `df::DataFrame`: The DataFrame containing the pressure and adsorption data for the isotherm
+- `pressure_col_name::Symbol`: The header of the pressure column. Can be found with `names(df)`
+- `loading_col_name::Symbol`: The header of the loading/adsorption column. Can be found with `names(df)`
+- `model::Symbol`: The model chosen to fit the isotherm
+
+# Returns
+- `params::Array{Float64, 1}`: Will contain the fitted parameters of the model chosen. `:langmuir` return M, K while `:henry` return K
+"""
 function fit_isotherm(df::DataFrame, pressure_col_name::Symbol, loading_col_name::Symbol, model::Symbol; henry_tol::Float64 = 0.25)
     sort!(df, [pressure_col_name])
     n = df[loading_col_name]
