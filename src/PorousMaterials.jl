22d3cffef9b178ea4b206a4037ab4df330c01318
SimonEnsemble <cory.simon@oregonstate.edu>
2020-02-09 09:26:04 -0800

    box refactored

--- src/PorousMaterials.jl
+++ src/PorousMaterials.jl
@@ -1,164 +1,165 @@
 module PorousMaterials
-
-using CSV
-using DataFrames
-using Roots # for fzero
-using OffsetArrays # used for Ewald sum
-using SpecialFunctions # for erfc
-using StatsBase
-using ProgressMeter
-using Polynomials
-using JLD2
-using Statistics
+ # 
+ # using CSV
+ # using DataFrames
+ # using Roots # for fzero
+ # using OffsetArrays # used for Ewald sum
+ # using SpecialFunctions # for erfc
+ # using StatsBase
+ # using ProgressMeter
+ # using Polynomials
+ # using JLD2
+ # using Statistics
 using Printf
 using LinearAlgebra
-using LightGraphs
-using Distributed
-using Optim
-import Base.push!
+ # using LightGraphs
+ # using Distributed
+ # using Optim
+ # import Base.push!
+ # 
 
-
-"""
-    print_file_paths()
-
-Print off paths where PorousMaterials.jl looks for input files and writes output files.
-"""
-function print_file_paths()
-    println("General data folder: ", PATH_TO_DATA)
-    println("\tcrystal structures (.cif, .cssr): ", PATH_TO_CRYSTALS)
-    println("\tforce field files (.csv): ", PATH_TO_FORCEFIELDS)
-    println("\tmolecule input files: ", PATH_TO_MOLECULES)
-    println("\tgrids (.cube): ", PATH_TO_GRIDS)
-end
-
-"""
-    set_default_file_paths(print_paths=true)
-
-Sets the default paths for where input files and some output files are stored.
-To see current set up, call [`print_file_paths`](@ref)
-"""
-function set_default_file_paths(;print_paths::Bool=true)
-    # this is the main directory where crystal structures, forcefields, and molecules data is stored
-    global PATH_TO_DATA = joinpath(pwd(), "data")
-
-    global PATH_TO_CRYSTALS = joinpath(PATH_TO_DATA, "crystals")
-    global PATH_TO_FORCEFIELDS = joinpath(PATH_TO_DATA, "forcefields")
-    global PATH_TO_MOLECULES = joinpath(PATH_TO_DATA, "molecules")
-    global PATH_TO_GRIDS = joinpath(PATH_TO_DATA, "grids")
-    
-    if print_paths
-        print_file_paths()
-    end
-end
-
-# this runs everytime PorousMaterials is loaded, so if the user changes directory
-#   then the PATH_TO_DATA will change as well
-function __init__()
-    set_default_file_paths(print_paths=false)
-end
-
-"""
-    set_tutorial_mode()
-
-Places PorousMaterials in "Tutorial Mode". It changes the `PATH_TO_DATA` variable to
-the directory where the PorousMaterials test data is stored. It can be used to
-follow examples shown in the README. It displays a warning so that the user knows
-They are no longer using their own data.
-"""
-function set_tutorial_mode()
-    new_path = joinpath(dirname(pathof(PorousMaterials)), "..", "test", "data")
-    if ! isdir(new_path)
-        @error @sprintf("Directory for testing data %s does not exist.\nNot entering Tutorial Mode.\n", new_path)
-    else
-        global PATH_TO_DATA = new_path
-        global PATH_TO_CRYSTALS = joinpath(PATH_TO_DATA, "crystals")
-        global PATH_TO_FORCEFIELDS = joinpath(PATH_TO_DATA, "forcefields")
-        global PATH_TO_MOLECULES = joinpath(PATH_TO_DATA, "molecules")
-        global PATH_TO_GRIDS = joinpath(PATH_TO_DATA, "grids")
-        @warn "PorousMaterials is now in Tutorial Mode. You have access to the testing data to experiment with PorousMaterials.\nTo reset to default file paths, call `set_default_file_paths()`\n"
-    end
-end
-
-include("Box.jl")
-include("Matter.jl")
-include("NearestImage.jl")
-include("Misc.jl")
-include("Crystal.jl")
-include("Molecules.jl")
-include("Forcefield.jl")
-include("Energetics_Util.jl")
-include("VdWEnergetics.jl")
-include("ElectrostaticEnergetics.jl")
-include("MChelpers.jl")
-include("Grid.jl")
-include("EOS.jl")
-include("Henry.jl")
-include("GCMC.jl")
-include("generic_rotations.jl")
+ # """
+ #     print_file_paths()
+ # 
+ # print off paths where porousmaterials.jl looks for input files and writes output files.
+ # """
+ # function print_file_paths()
+ #     println("general data folder: ", path_to_data)
+ #     println("\tcrystal structures (.cif, .cssr): ", path_to_crystals)
+ #     println("\tforce field files (.csv): ", path_to_forcefields)
+ #     println("\tmolecule input files: ", path_to_molecules)
+ #     println("\tgrids (.cube): ", path_to_grids)
+ # end
+ # 
+ # """
+ #     set_default_file_paths(print_paths=true)
+ # 
+ # sets the default paths for where input files and some output files are stored.
+ # to see current set up, call [`print_file_paths`](@ref)
+ # """
+ # function set_default_file_paths(;print_paths::bool=true)
+ #     # this is the main directory where crystal structures, forcefields, and molecules data is stored
+ #     global path_to_data = joinpath(pwd(), "data")
+ # 
+ #     global path_to_crystals = joinpath(path_to_data, "crystals")
+ #     global path_to_forcefields = joinpath(path_to_data, "forcefields")
+ #     global path_to_molecules = joinpath(path_to_data, "molecules")
+ #     global path_to_grids = joinpath(path_to_data, "grids")
+ #     
+ #     if print_paths
+ #         print_file_paths()
+ #     end
+ # end
+ # 
+ # # this runs everytime porousmaterials is loaded, so if the user changes directory
+ # #   then the path_to_data will change as well
+ # function __init__()
+ #     set_default_file_paths(print_paths=false)
+ # end
+ # 
+ # """
+ #     set_tutorial_mode()
+ # 
+ # places porousmaterials in "tutorial mode". it changes the `path_to_data` variable to
+ # the directory where the porousmaterials test data is stored. it can be used to
+ # follow examples shown in the readme. it displays a warning so that the user knows
+ # they are no longer using their own data.
+ # """
+ # function set_tutorial_mode()
+ #     new_path = joinpath(dirname(pathof(porousmaterials)), "..", "test", "data")
+ #     if ! isdir(new_path)
+ #         @error @sprintf("directory for testing data %s does not exist.\nnot entering tutorial mode.\n", new_path)
+ #     else
+ #         global path_to_data = new_path
+ #         global path_to_crystals = joinpath(path_to_data, "crystals")
+ #         global path_to_forcefields = joinpath(path_to_data, "forcefields")
+ #         global path_to_molecules = joinpath(path_to_data, "molecules")
+ #         global path_to_grids = joinpath(path_to_data, "grids")
+ #         @warn "porousmaterials is now in tutorial mode. you have access to the testing data to experiment with porousmaterials.\nto reset to default file paths, call `set_default_file_paths()`\n"
+ #     end
+ # end
+ # 
+include("matter.jl")
+include("box.jl")
+ # include("nearestimage.jl")
+ # include("misc.jl")
+ # include("crystal.jl")
+ # include("molecules.jl")
+ # include("forcefield.jl")
+ # include("energetics_util.jl")
+ # include("vdwenergetics.jl")
+ # include("electrostaticenergetics.jl")
+ # include("mchelpers.jl")
+ # include("grid.jl")
+ # include("eos.jl")
+ # include("henry.jl")
+ # include("gcmc.jl")
+ # include("generic_rotations.jl")
 
 export
-    # PorousMaterials.jl
-    set_default_file_paths, print_file_paths, set_tutorial_mode,
+ #     # porousmaterials.jl
+ #     set_default_file_paths, print_file_paths, set_tutorial_mode,
+ # 
+    # matter.jl
+    Coords, Frac, Cart, Atoms, Charges,
+    
+    # box.jl
+    Box, replicate, unit_cube, write_vtk, inside, fractional_coords, cartesian_coords
 
-    # Box.jl
-    Box, replicate, UnitCube, write_vtk, inside,
-
-    # Matter.jl
-    Atoms, Charges, has_same_set_of_atoms, has_same_set_of_charges,
-
-    # NearestImage.jl
-    nearest_image!, nearest_r², nearest_r,
-
-    # Misc.jl
-    read_xyz, read_cpk_colors, read_atomic_radii, write_xyz, fit_adsorption_isotherm,
-
-    # Crystal.jl
-    Framework, BondingRule, read_crystal_structure_file,
-    remove_overlapping_atoms_and_charges, strip_numbers_from_atom_labels!,
-    chemical_formula, molecular_weight, crystal_density, construct_box,
-    replicate, read_atomic_masses, charged, write_cif, assign_charges,
-    is_symmetry_equal, apply_symmetry_rules, assert_P1_symmetry, infer_bonds!,
-    remove_bonds!, compare_bonds_in_framework, wrap_atoms_to_unit_cell!,
-    write_bond_information, is_bonded, default_bondingrules, has_same_sets_of_atoms_and_charges, 
-    distance, bond_sanity_check,
-
-    # Molecules.jl
-    Molecule, n_atoms, set_fractional_coords!, translate_by!, outside_box,
-    set_fractional_coords_to_unit_cube!, translate_to!, rotate!, rotation_matrix,
-    rand_point_on_unit_sphere, charged, pairwise_atom_distances,
-    pairwise_charge_distances, Ion, bond_length_drift,
-
-    # Forcefield.jl
-    LJForceField, replication_factors, check_forcefield_coverage,
-
-    # Energetics_Util.jl
-    PotentialEnergy, SystemPotentialEnergy,
-
-    # VdWEnergetics.jl
-    lennard_jones, vdw_energy, vdw_energy_no_PBC,
-
-    # ElectrostaticEnergetics.jl
-    electrostatic_potential, electrostatic_potential_energy, precompute_kvec_wts,
-    setup_Ewald_sum, total, Eikr, total_electrostatic_potential_energy,
-
-    # MChelpers.jl
-    insert_molecule!, delete_molecule!, translate_molecule!, reinsert_molecule!, rotatable,
-
-    # Grid.jl
-    apply_periodic_boundary_condition!,
-    Grid, write_cube, read_cube, energy_grid, compute_accessibility_grid, accessible,
-    required_n_pts, xf_to_id, id_to_xf, update_density!,
-
-    # EOS.jl
-    calculate_properties, PengRobinsonFluid, VdWFluid,
-
-    # GCMC.jl
-    gcmc_simulation, adsorption_isotherm, stepwise_adsorption_isotherm,
-    gcmc_result_savename, GCMCstats, MarkovCounts,
-
-    # Henry.jl
-    henry_coefficient, henry_result_savename,
-
-    # generic_rotations.jl
-    rotation_matrix
+ # 
+ #     # nearestimage.jl
+ #     nearest_image!, nearest_r², nearest_r,
+ # 
+ #     # misc.jl
+ #     read_xyz, read_cpk_colors, read_atomic_radii, write_xyz, fit_adsorption_isotherm,
+ # 
+ #     # crystal.jl
+ #     framework, bondingrule, read_crystal_structure_file,
+ #     remove_overlapping_atoms_and_charges, strip_numbers_from_atom_labels!,
+ #     chemical_formula, molecular_weight, crystal_density, construct_box,
+ #     replicate, read_atomic_masses, charged, write_cif, assign_charges,
+ #     is_symmetry_equal, apply_symmetry_rules, assert_p1_symmetry, infer_bonds!,
+ #     remove_bonds!, compare_bonds_in_framework, wrap_atoms_to_unit_cell!,
+ #     write_bond_information, is_bonded, default_bondingrules, has_same_sets_of_atoms_and_charges, 
+ #     distance, bond_sanity_check,
+ # 
+ #     # molecules.jl
+ #     molecule, n_atoms, set_fractional_coords!, translate_by!, outside_box,
+ #     set_fractional_coords_to_unit_cube!, translate_to!, rotate!, rotation_matrix,
+ #     rand_point_on_unit_sphere, charged, pairwise_atom_distances,
+ #     pairwise_charge_distances, ion, bond_length_drift,
+ # 
+ #     # Forcefield.jl
+ #     LJForceField, replication_factors, check_forcefield_coverage,
+ # 
+ #     # Energetics_Util.jl
+ #     PotentialEnergy, SystemPotentialEnergy,
+ # 
+ #     # VdWEnergetics.jl
+ #     lennard_jones, vdw_energy, vdw_energy_no_PBC,
+ # 
+ #     # ElectrostaticEnergetics.jl
+ #     electrostatic_potential, electrostatic_potential_energy, precompute_kvec_wts,
+ #     setup_Ewald_sum, total, Eikr, total_electrostatic_potential_energy,
+ # 
+ #     # MChelpers.jl
+ #     insert_molecule!, delete_molecule!, translate_molecule!, reinsert_molecule!, rotatable,
+ # 
+ #     # Grid.jl
+ #     apply_periodic_boundary_condition!,
+ #     Grid, write_cube, read_cube, energy_grid, compute_accessibility_grid, accessible,
+ #     required_n_pts, xf_to_id, id_to_xf, update_density!,
+ # 
+ #     # EOS.jl
+ #     calculate_properties, PengRobinsonFluid, VdWFluid,
+ # 
+ #     # GCMC.jl
+ #     gcmc_simulation, adsorption_isotherm, stepwise_adsorption_isotherm,
+ #     gcmc_result_savename, GCMCstats, MarkovCounts,
+ # 
+ #     # Henry.jl
+ #     henry_coefficient, henry_result_savename,
+ # 
+ #     # generic_rotations.jl
+ #     rotation_matrix
 end
