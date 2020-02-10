43461b290e0c9085923fbf902fab9360a7552612
Arthur York <ahyork98@gmail.com>
2020-02-09 17:25:10 -0800

    added tests for framework partitioning

--- test/crystal_test.jl
+++ test/crystal_test.jl
@@ -237,6 +237,21 @@ using Random
     @test isapprox(f4.atoms, f1.atoms + f2.atoms + f3.atoms)
     @test isapprox(f4.charges, f1.charges + f2.charges + f3.charges)
 
+    # test framework partitioning
+    sbmof1 = Framework("SBMOF-1.cif")
+    # create 3 partitions to test splitting the framework
+    #   there are 120 atoms in sbmof1, so each partition will have 40 atoms
+    lo = collect(1:40)
+    mid = collect(41:80)
+    hi = collect(81:120)
+    sbmof1_partitions = partition_framework(sbmof1, [lo, mid, hi])
+    @test length(lo) == sbmof1_partitions[1].atoms.n_atoms
+    @test length(mid) == sbmof1_partitions[2].atoms.n_atoms
+    @test length(hi) == sbmof1_partitions[3].atoms.n_atoms
+    # add the framework partitions back together
+    sbmof1_rebuilt = +(sbmof1_partitions...)
+    @test isapprox(sbmof1, sbmof1_rebuilt)
+
     # more xtal tests
     sbmof1 = Framework("SBMOF-1.cif")
     @test !charged(sbmof1)
