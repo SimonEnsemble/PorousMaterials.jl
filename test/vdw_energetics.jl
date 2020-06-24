24811715383702915ceace0de538f7172f2f2c40
SimonEnsemble <cory.simon@oregonstate.edu>
2020-06-23 22:58:52 -0700

    gg vdw tests

--- test/vdw_energetics.jl
+++ test/vdw_energetics.jl
@@ -86,5 +86,112 @@ using Random
     atoms = read_xyz("data/crystals/CB5.xyz") # raw .xyz of cage
     co2 = Molecule("CO2")
     @test isapprox(energy, vdw_energy_no_PBC(atoms, co2.atoms, ljff))
+
+    ###
+    #   guest-guest energetics
+    ###
+    ljff = LJForceField("Dreiding", r_cutoff=12.5)
+    box = Box(25.0, 25.0, 25.0, π/2, π/2, π/2)
+    # a He and Xe a distance of 6.0 away
+    xe = Frac(Molecule("Xe"), box)
+    he = Frac(Molecule("He"), box)
+    translate_to!(xe, Cart([5.0, 12.0, 12.0]), box)
+    translate_to!(he, Cart([11.0, 12.0, 12.0]), box)
+    molecules = [xe, he]
+    r² = (11.0 - 5.0) ^ 2 # duh
+    energy = lennard_jones(r², ljff.σ²[:Xe][:He], ljff.ϵ[:Xe][:He])
+    @test energy ≈ vdw_energy(1, molecules, ljff, box)
+    @test energy ≈ vdw_energy(2, molecules, ljff, box) # symmetry
+
+    # via PBC, a distance (24.0 - 5.0) > (1+5)
+    translate_to!(molecules[2], Cart([24.0, 12.0, 12.0]), box)
+    r² = (1.0 + 5.0) ^ 2 # PBC
+    energy = lennard_jones(r², ljff.σ²[:Xe][:He], ljff.ϵ[:He][:Xe])
+    @test energy ≈ vdw_energy(2, molecules, ljff, box)
+    @test energy ≈ vdw_energy(1, molecules, ljff, box) # symmetry again.
+
+    # put a molecule on top of first one.
+    push!(molecules, deepcopy(molecules[1]))
+    @test vdw_energy(2, molecules, ljff, box) ≈ 2 * energy
+
+    @test vdw_energy(1, molecules, ljff, box) == Inf
+    @test vdw_energy(3, molecules, ljff, box) == Inf
+
+    # interaction energy between first and second should be same via PBC
+    molecules_a = Frac.([Molecule("Xe"), Molecule("He")], box)
+    translate_to!(molecules_a[1], Cart([11.0, 1.0, 12.0]), box)
+    translate_to!(molecules_a[2], Cart([11.0, 4.0, 12.0]), box)
+    molecules_b = Frac.([Molecule("Xe"), Molecule("He")], box)
+    translate_to!(molecules_b[1], Cart([11.0, 1.0, 12.0]), box)
+    translate_to!(molecules_b[2], Cart([11.0, 23.0, 12.0]), box)
+    @test vdw_energy(1, molecules_a, ljff, box) ≈ vdw_energy(1, molecules_b, ljff, box)
+
+    # another PBC one where three coords are different.
+    molecules = Frac.([Molecule("Xe"), Molecule("He")], box)
+    translate_to!(molecules[1], Cart([24.0, 23.0, 11.0]), box)
+    translate_to!(molecules[2], Cart([22.0, 2.0, 12.0]), box)
+    r² = 4.0^2 + 2.0^2 + 1.0^2
+    energy = lennard_jones(r², ljff.σ²[:He][:Xe], ljff.ϵ[:He][:Xe])
+    @test vdw_energy(1, molecules, ljff, box) ≈ energy
+    @test vdw_energy(2, molecules, ljff, box) ≈ energy
+
+    # test cutoff radius. molecules here are too far to interact
+    translate_to!(molecules[1], Cart([0.0, 0.0, 0.0]), box)
+    translate_to!(molecules[2], Cart([12.0, 12.0, 12.0]), box)
+    @test vdw_energy(1, molecules, ljff, box) ≈ 0.0
+    @test vdw_energy(2, molecules, ljff, box) ≈ 0.0
+    # the position of a molecule should not change inside vdw_energy.
+    @test all(molecules[1].atoms.coords.xf[:, 1] .== box.c_to_f * [0.0, 0.0, 0.0])
+    @test all(molecules[2].atoms.coords.xf[:, 1] .== box.c_to_f * [12.0, 12.0, 12.0])
+    # TODO write tests for CO2 where there are more than one beads
+
+    # Molecules with more than one ljsphere
+
+    # two CO2 molecules 6.0 units apart
+    molecules_co2 = Frac.([Molecule("CO2"), Molecule("CO2")], box)
+    translate_to!(molecules_co2[1], Cart([12.0, 9.0, 12.0]), box)
+    translate_to!(molecules_co2[2], Cart([12.0, 15.0, 12.0]), box)
+    # because the molecules have not been rotated, all corresponding beads are same
+    #   distance apart when they are separated along the y-axis
+    r²_com = (15.0 - 9.0)^2
+    # distance between teh central carbon and an oxygen in one molecule this
+    #   takes advantage of the fact that the carbon is the central atom, and that
+    #   all three atoms are in a line
+    r²_co = 1.16^2
+    # distance between the two oxygens in one molecule
+    r²_oo = (2.0 * 1.16)^2
+    energy = (2.0 * lennard_jones(r²_com, ljff.σ²[:O_CO2][:O_CO2], ljff.ϵ[:O_CO2][:O_CO2])
+        + 4.0 * lennard_jones(r²_com + r²_co, ljff.σ²[:O_CO2][:C_CO2], ljff.ϵ[:O_CO2][:C_CO2])
+        + 2.0 * lennard_jones(r²_com + r²_oo, ljff.σ²[:O_CO2][:O_CO2], ljff.ϵ[:O_CO2][:O_CO2])
+        + lennard_jones(r²_com, ljff.σ²[:C_CO2][:C_CO2], ljff.ϵ[:C_CO2][:C_CO2]))
+    @test vdw_energy(1, molecules_co2, ljff, box) ≈ energy
+    @test vdw_energy(2, molecules_co2, ljff, box) ≈ energy
+
+    # PBC placing one at 2.0 and the other at 21.0
+    translate_to!(molecules_co2[1], Cart([12.0, 2.0, 12.0]), box)
+    translate_to!(molecules_co2[2], Cart([12.0, 21.0, 12.0]), box)
+    r²_com = (4.0 + 2.0)^2
+    energy = (2.0 * lennard_jones(r²_com, ljff.σ²[:O_CO2][:O_CO2], ljff.ϵ[:O_CO2][:O_CO2])
+        + 4.0 * lennard_jones(r²_com + r²_co, ljff.σ²[:O_CO2][:C_CO2], ljff.ϵ[:O_CO2][:C_CO2])
+        + 2.0 * lennard_jones(r²_com + r²_oo, ljff.σ²[:O_CO2][:O_CO2], ljff.ϵ[:O_CO2][:O_CO2])
+        + lennard_jones(r²_com, ljff.σ²[:C_CO2][:C_CO2], ljff.ϵ[:C_CO2][:C_CO2]))
+    @test vdw_energy(1, molecules_co2, ljff, box) ≈ energy
+    @test vdw_energy(2, molecules_co2, ljff, box) ≈ energy
+
+    # testing cutoff radius, so only one oxygen from each will be able to interact
+    # making a larger box so that only a few.atoms from each CO2 will be able to interact
+    box_large = Box(50.0, 50.0, 50.0, π/2, π/2, π/2)
+    molecules_co2 = Frac.([Molecule("CO2"), Molecule("CO2")], box_large)
+    # placed 12.6 units apart so the C atoms will be outside the cutoff radius,
+    #   but one O atom from each will be inside, so these will interact
+    translate_to!(molecules_co2[1], Cart([0.0, 0.0, 0.0]), box_large)
+    translate_to!(molecules_co2[2], Cart([13.0, 0.0, 0.0]), box_large)
+    r²_com = (13.0)^2
+    r²_o = (13.0 - (2.0 * 1.16))^2
+    r²_co = (13.0 - 1.16)^2
+    energy = (lennard_jones(r²_o, ljff.σ²[:O_CO2][:O_CO2], ljff.ϵ[:O_CO2][:O_CO2])
+        + 2 * lennard_jones(r²_co, ljff.σ²[:O_CO2][:C_CO2], ljff.ϵ[:O_CO2][:C_CO2]))
+    @test vdw_energy(1, molecules_co2, ljff, box_large) ≈ energy
+    @test vdw_energy(2, molecules_co2, ljff, box_large) ≈ energy
 end
 end
