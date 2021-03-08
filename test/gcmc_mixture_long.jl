5d8f3a22c56f20cc519e0d98b056d4d4cc789ff3
Nickolas Gantzler <gantzlen@oregonstate.edu>
2021-03-08 14:45:08 -0800

    added RASPA data to test/data dir to benchmark the GCMC mixture simulations. wrote the tests for GCMC mixture simulations, but waiting on more accurate simlation to finish

--- test/gcmc_mixture_long.jl
+++ test/gcmc_mixture_long.jl
@@ -1,9 +1,12 @@
 using PorousMaterials
 using Test
 using Printf
+using CSV
+using DataFrames
+using JLD2
 
-tests_to_run = Dict("Kr/Xe in SBMOF-1" => false,
-                    "ideal gas mixture" => true
+tests_to_run = Dict("Kr/Xe in SBMOF-1" => true,
+                    "ideal gas mixture" => false
                     )
 ###
 #  Simulate Kr/Xe mixture in SBMOF-1
@@ -12,15 +15,50 @@ if tests_to_run["Kr/Xe in SBMOF-1"]
     @info "running Kr/Xe in SBMOF-1 test"
     # set up sim
     xtal            = Crystal("SBMOF-1.cif")
-    molecules       = [Molecule("Kr"), Molecule("Xe")]
+    adsorbates      = ["Kr", "Xe"]
+    mol_templates   = Molecule.(adsorbates)
     ljff            = LJForceField("UFF", mixing_rules="Lorentz-Berthelot")
     temperature     = 298.0
-    n_sample_cycles = 50000
-    n_burn_cycles   = 50000
-    pressures       = [0.8, 0.2]
-    # run sim
-    results, molecules = μVT_sim(xtal, molecules, temperature, pressures, ljff, n_burn_cycles=n_burn_cycles, n_sample_cycles=n_sample_cycles)
-    # TODO: evaluate results.
+    n_sample_cycles = 500000
+    n_burn_cycles   = 500000
+    pressures       = [0.9, 0.1]
+    # load RASPA (benchmark) data
+    raspa_data_filename = joinpath(PorousMaterials.PATH_TO_DATA, "raspa_data/Kr_Xe.csv")
+    raspa_results = CSV.read(raspa_data_filename, DataFrame)
+    # run sim over range of total pressures
+    for (i, p) in enumerate([0.01, 0.1, 0.5])
+#    results, molecules = μVT_sim(xtal,
+#                                 mol_templates,
+#                                 temperature,
+#                                 p *  pressures,
+#                                 ljff,
+#                                 n_burn_cycles=n_burn_cycles,
+#                                 n_sample_cycles=n_sample_cycles)
+
+        filename = μVT_output_filename(xtal, mol_templates, temperature, 
+                                   p * pressures, ljff, n_burn_cycles, 
+                                   n_sample_cycles; comment="", extension=".jld2")
+        where_are_jld_files = PorousMaterials.PATH_TO_SIMS
+        @load joinpath(where_are_jld_files, filename) results
+
+        # evaluate results
+        for (j, sp) in enumerate(adsorbates)
+            @info "pressure = $p \n adsorbate = $sp"
+            # make sure the pressures are correct
+            @test results["pressure (bar)"][j] ≈ raspa_results[i, Symbol(sp * " pressure (bar)")]
+            # evaluate adsorption uptake
+            std_err = raspa_results[i, Symbol("err $sp loading (mmol/g)")] + results["err ⟨N⟩ (mmol/g)"][j]
+            @test isapprox(raspa_results[i, Symbol(sp * " loading (mmol/g)")], results["⟨N⟩ (mmol/g)"][j], atol=std_err)
+            @info "passed"
+#            if results["⟨N⟩ (mmol/g)"][j] < raspa_results[i, Symbol(sp * " loading (mmol/g)")]
+#                r_min = raspa_results[i, Symbol(sp * " loading (mmol/g)")] - raspa_results[i, Symbol("err " * sp * " loading (mmol/g)")]
+#                @test r_min <= (results["⟨N⟩ (mmol/g)"][j] + results["err ⟨N⟩ (mmol/g)"][j])
+#            else
+#                r_max = raspa_results[i, Symbol(sp * " loading (mmol/g)")] + raspa_results[i, Symbol("err " * sp * " loading (mmol/g)")]
+#                @test (results["⟨N⟩ (mmol/g)"][j] - results["err ⟨N⟩ (mmol/g)"][j]) <= r_max
+#            end 
+        end
+    end
 end
 
 ###
@@ -45,16 +83,22 @@ if tests_to_run["ideal gas mixture"]
     n_ig  = partial_fugacities * empty_space.box.Ω / (PorousMaterials.KB * temperature) * 100000.0
     n_sim = similar(n_ig)
     for i in 1:length(partial_fugacities)
-        results, molecules = μVT_sim(empty_space, [ideal_gas1, ideal_gas2], temperature, partial_fugacities[i], forcefield,
-                                     n_burn_cycles=500000, n_sample_cycles=500000, verbose=true)
-
+        results, molecules = μVT_sim(empty_space, 
+                                     [ideal_gas1, ideal_gas2], 
+                                     temperature, 
+                                     partial_fugacities[i], 
+                                     forcefield,
+                                     n_burn_cycles=500000, 
+                                     n_sample_cycles=500000, 
+                                     verbose=false)
+        # print fugacities 
         for s in 1:2
             @printf("\nfugacities %f, n_ig = %f, n_sim = %f\n", 
                     partial_fugacities[i][s], n_ig[i][s], results["⟨N⟩ (molecules/unit cell)"][s])
         end
+        # test results
         @test isapprox(sum(results["⟨N⟩ (molecules/unit cell)"]), sum(n_ig[i]), rtol=0.05)
         @test isapprox(results["⟨N⟩ (molecules/unit cell)"][1], n_ig[i][1], rtol=0.05)
         @test isapprox(results["⟨N⟩ (molecules/unit cell)"][2], n_ig[i][2], rtol=0.05)
     end
 end
-
